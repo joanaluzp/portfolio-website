9380a10711bfc59c449240b605bb8cf99f849075
joanaluzp <joanaluzp@gmail.com>
2024-08-26 17:52:32 +0100

    feat: adding random background img and applying svg filter

--- components/BackgroundImg.vue
+++ components/BackgroundImg.vue
@@ -1,22 +1,28 @@
 <template>
-  <section class="section section-background homepage">
-    <div v-if="randomItem" class="background-img">
-      <template v-if="randomItem.images.length > 0">
-        <div
-          class="background-img-item"
-          :style="{
-            'background-image': 'url(' + randomItem.images[0].image + ')',
-          }"
-        ></div>
-      </template>
+  <section v-if="!hideBackground" class="section section-background homepage">
+    <div v-if="randomImage" class="background-img">
+      <img
+        class="background-img-item"
+        style="filter: url(#svgFilter)"
+        :src="randomImage"
+      />
     </div>
   </section>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
+import { useRoute } from "vue-router";
+
 let categoryItems = ref([]);
 const randomItem = ref(null);
+const randomImage = ref(null);
+const route = useRoute();
+
+const hideBackground = computed(() => {
+  const id = route.params.id; 
+  return route.path === "/" || route.path === `/work/${id}`;
+});
 
 const props = defineProps({
   database: {
@@ -27,20 +33,37 @@ const props = defineProps({
 
 const filteredCategoryItems = computed(() => {
   return categoryItems.value.filter((item) => {
-    return (
-      item.images.length > 0 ||
-      item.category === "front-end development" ||
-      (item.images.length > 0 && item.category === "video art")
-    );
+    return item.images && item.images.length > 0;
   });
 });
 
-onMounted(() => {
-  categoryItems.value = props.database.data.work.categoryItem;
+const selectRandomItem = () => {
   const filteredItems = filteredCategoryItems.value;
   if (filteredItems.length > 0) {
     randomItem.value =
       filteredItems[Math.floor(Math.random() * filteredItems.length)];
+    if (randomItem.value.images.length > 0) {
+      randomImage.value =
+        randomItem.value.images[
+          Math.floor(Math.random() * randomItem.value.images.length)
+        ].image;
+    }
+  }
+};
+
+watch(
+  () => route.path,
+  () => {
+    if (!hideBackground.value) {
+      selectRandomItem();
+    }
+  }
+);
+
+onMounted(() => {
+  categoryItems.value = props.database.data.work.categoryItem;
+  if (!hideBackground.value) {
+    selectRandomItem();
   }
 });
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,33 @@
   <div class="app-content" v-else>
     <Navbar :database="database" />
     <main class="main">
+      <svg xmlns="" style="display: none">
+        <defs>
+          <filter
+            id="svgFilter"
+            x="-20%"
+            y="-20%"
+            width="140%"
+            height="140%"
+            filterUnits="objectBoundingBox"
+            primitiveUnits="userSpaceOnUse"
+            color-interpolation-filters="linearRGB"
+          >
+            <feDisplacementMap
+              in="SourceGraphic"
+              in2="blur"
+              scale="150"
+              xChannelSelector="R"
+              yChannelSelector="B"
+              x="0%"
+              y="0%"
+              width="100%"
+              height="100%"
+              result="displacementMap1"
+            />
+          </filter>
+        </defs>
+      </svg>
       <NuxtPage :database="database" />
       <BackgroundImg :database="database" />
     </main>
@@ -14,7 +41,6 @@
 <script setup>
 import database from "../data/db.json";
 const maintenance = ref(true);
-
 </script>
 <style>
 .page-enter-active,
